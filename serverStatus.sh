31cdd8170b3fc5f4ffce0ce59a336d12c8b2603a
Eric Erfanian <eric.erfanian@gmail.com>
2013-01-31 17:45:33 -0600

    breaking up the script into functions and adding the ability to set the signal regardless of test order

--- serverStatus.sh
+++ serverStatus.sh
@@ -6,26 +6,44 @@
 #Enter a list of the hostnames or IP Addresses you want to check using fping.
 HOSTS="hostname1 ipaddress2"
 #Enter a list of addresses you wish to check with curl.
-HOSTS2="ip:port ip2:port2"
+HOSTS2="ip1:port1 ip2:port2"
 #Get the physical address of the USB blink(1) dongle
 USB_ID=$(lsusb -d 27b8:01ed)
+#Set a global variable to track if one server is down
+RESULT=0
+
+#Set the flare:
+function lightOn ()
+{
+  echo $h down
+	  #turn on the alert light to bright red
+          ./blink1-tool -m 100 --rgb 255,0,0
+          #reset the USB due to some issues on my virtual machine
+          ./reset /dev/bus/usb/${USB_ID:4:3}/${USB_ID:15:3}
+#Break from the test loops because we know something is wrong.
+  RESULT=1
+}
+
+#Else Quiet
+function lightOff ()
+{
+  if [ $RESULT -ne 1 ]; then
+   echo "hosts up"
+   #turn off the alert light
+   ./blink1-tool --off
+   #reset the USB due to some issues on my virtual machine
+   ./reset /dev/bus/usb/${USB_ID:4:3}/${USB_ID:15:3}
+  fi
+}
 
 #Test HOSTS with fping.
 for h in $HOSTS
 do
   fping -u $h >& /dev/null
   if [ $? -ne 1 ]; then
-          echo "hosts up"
-          #turn off the alert light
-          ./blink1-tool --off
-          #reset the USB due to some issues on my virtual machine
-          ./reset /dev/bus/usb/${USB_ID:4:3}/${USB_ID:15:3}
+          lightOff
   else
-	  echo ${h} down
-	  #turn on the alert light to bright red
-          ./blink1-tool -m 100 --rgb 255,0,0
-          #reset the USB due to some issues on my virtual machine
-          ./reset /dev/bus/usb/${USB_ID:4:3}/${USB_ID:15:3}	  
+	  lightOn $h 
   fi
 done
 
@@ -34,16 +52,9 @@ for h in $HOSTS2
 do
   curl $h >& /dev/null
   if [ $? -ne 7 ]; then
-          echo "hosts up"
-          #turn off the alert light
-          ./blink1-tool --off
-          #reset the USB due to some issues on my virtual machine
-          ./reset /dev/bus/usb/${USB_ID:4:3}/${USB_ID:15:3}
+          lightOff
   else
-	  echo ${h} down
-	  #turn on the alert light to bright red
-          ./blink1-tool -m 100 --rgb 255,0,0
-          #reset the USB due to some issues on my virtual machine
-          ./reset /dev/bus/usb/${USB_ID:4:3}/${USB_ID:15:3}	  
+	  lightOn $h
   fi
 done
+
